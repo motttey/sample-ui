01464ed45be2c29872da7b9d3288a1ad43d3eefc
motttey <motitago@gmail.com>
2024-10-15 22:55:21 +0900

    install: ts and eslint settings

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import reactPlugin from 'eslint-plugin-react';
+import storybookPlugin from 'eslint-plugin-storybook';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ["node_modules"],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
+  },
+  {
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+    },
+    rules: {
+      // TypeScript 用のルールをここに追加できます
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    plugins: {
+      storybook: storybookPlugin,
+    },
+    rules: {
+      // Storybook recommended rules (手動で定義)
+      ...storybookPlugin.configs.recommended.rules,
+    },
+  },
+];
