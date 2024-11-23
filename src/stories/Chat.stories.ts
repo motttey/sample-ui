7260f7290030944219ccf6179afda897a70fa292
motttey <motitago@gmail.com>
2024-11-23 21:26:32 +0900

    modify: add sample stories

--- src/stories/Chat.stories.ts
+++ src/stories/Chat.stories.ts
@@ -42,9 +42,99 @@ const defaultArgs = {
   ],
 }
 
+
+const chat1Arg = {
+  title: '地元 アホ',
+  messageList: [
+    {
+      text: 'おいハンバーグ! 👊',
+      sender: 'bot',
+      hasImage: true,
+    },
+    {
+      text: '最近お前地元で見ねーけど何してるん？ 笑',
+      sender: 'bot',
+      hasImage: false,
+    },
+    {
+      text: '今週末パイゼで集合な笑',
+      sender: 'bot',
+      hasImage: false,
+    },
+  ],
+  senderImgUrl: "https://placehold.jp/3d4070/ffffff/150x150.png?text=%20",
+  receiverImgUrl: "https://placehold.jp/999999/ffffff/150x150.png?text=%20",
+}
+
+const chat2Arg = {
+  title: '地元 アホ',
+  messageList: [
+    {
+      text: 'おひさ！同窓会でもオマエの話題でてたわ笑',
+      sender: 'bot',
+      hasImage: true,
+    },
+    {
+      text: '6月6日に地元メンツでBBQすっから、今回は顔出せよ～！',
+      sender: 'bot',
+      hasImage: false,
+    },
+    {
+      text: 'マジ来なかったら後悔するぞ👊👊👊',
+      sender: 'bot',
+      hasImage: false,
+    },
+  ],
+  senderImgUrl: "https://placehold.jp/3d4070/ffffff/150x150.png?text=%20",
+  receiverImgUrl: "https://placehold.jp/999999/ffffff/150x150.png?text=%20",
+}
+
+
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default = {
   args: {
     ...defaultArgs,
+  },
+};
+
+export const Chat1_1 = {
+  args: {
+    ...chat1Arg,
+  },
+};
+
+
+export const Chat1_2 = {
+  args: {
+    ...chat1Arg,
+    messageList: [
+      ...chat1Arg.messageList,
+      {
+        text: '行きません',
+        sender: 'user',
+        hasImage: true,
+      }
+    ],
+  },
+};
+
+export const Chat2_1 = {
+  args: {
+    ...chat2Arg,
+  },
+};
+
+
+export const Chat2_2 = {
+  args: {
+    ...chat2Arg,
+    messageList: [
+      ...chat2Arg.messageList,
+      {
+        text: '行きます...',
+        sender: 'user',
+        hasImage: true,
+      }
+    ]
   },
 };
