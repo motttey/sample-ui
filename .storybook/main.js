48361d08650d54f4dc413bad4088a0dc3a08bb41
motttey <motitago@gmail.com>
2024-11-25 21:32:12 +0900

    modify: migrate storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,14 +2,23 @@
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,16 @@
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-    parameters: {
-      controls: {
-        matchers: {
-          color: /(background|color)$/i,
-          date: /Date$/,
-        },
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
       },
     },
-  };
+  },
+
+  tags: ["autodocs"]
+};
   
   export default preview;
   
